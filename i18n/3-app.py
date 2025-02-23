b3258f2554f50e6e0b2218d1b83704b09488a798
ajokatem <a.biar@alustudent.com>
2025-02-23 23:28:49 +0200

    i18n

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -35,4 +35,4 @@ def index():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port) 
+    app.run(host=host, port=port)
